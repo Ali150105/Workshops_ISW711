c96d581c0c59df2ae2d790d3a3c4dec7a8b16591
Ali_1501_2005 <anoelia1501@gmail.com>
2025-02-12 17:19:56 -0600

    cambios a la configuración

--- Workshop_1/index.js
+++ Workshop_1/index.js
@@ -1,10 +1,13 @@
 const express = require('express');
-const mongoose = require('mongoose');
-
 const app = express();
 
-app.use(express.json());
+// Ruta principal
+app.get('/', (req, res) => {
+    res.send('¡Hola, mundo desde Node.js!');
+});
 
-app.listen(3000, () => {
-    console.log(`Server Started at ${3000}`)
-})
+// Puerto del servidor
+const PORT = 3000;
+app.listen(PORT, () => {
+    console.log(`Servidor ejecutándose en http://localhost:${PORT}`);
+});
