dc905ad9a8f02a74064b0f9e01a38a4de5338005
Ali_1501_2005 <anoelia1501@gmail.com>
2025-02-13 12:48:00 -0600

    workshop 1 con bd funcional

--- Workshop_1/index.js
+++ Workshop_1/index.js
@@ -1,13 +1,27 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const app = express();
+app.use(express.json());
 
-// Ruta principal
-app.get('/', (req, res) => {
-    res.send('¡Hola, mundo desde Node.js!');
-});
+app.listen(3000, () => {
+    console.log(`Server Started at ${3000}`)
+})
 
-// Puerto del servidor
-const PORT = 3000;
-app.listen(PORT, () => {
-    console.log(`Servidor ejecutándose en http://localhost:${PORT}`);
-});
+require('dotenv').config();
+
+const mongoString = process.env.DATABASE_URL
+
+mongoose.connect(mongoString);
+const database = mongoose.connection
+
+database.on('error', (error) => {
+    console.log(error)
+})
+
+database.once('connected', () => {
+    console.log('Database Connected');
+})
+
+const routes = require('./routes/routes');
+
+app.use('/api', routes)
