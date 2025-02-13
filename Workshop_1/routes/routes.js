6a1188b3d8188372c4f81deff7c7e5ee4949f4d9
Ali_1501_2005 <anoelia1501@gmail.com>
2025-02-13 13:04:28 -0600

    w1 terminado y corregido

--- Workshop_1/routes/routes.js
+++ Workshop_1/routes/routes.js
@@ -16,7 +16,7 @@ router.get('/getAll', (req, res) => {
 
 //Get by ID Method
 router.get('/getOne/:id', (req, res) => {
-    res.send('Get by ID API')
+    res.send(req.params.id)
 })
 
 //Update by ID Method
@@ -27,4 +27,111 @@ router.patch('/update/:id', (req, res) => {
 //Delete by ID Method
 router.delete('/delete/:id', (req, res) => {
     res.send('Delete by ID API')
+})
+//meter datos en DB
+const Model = require('../modelo/model');
+
+//publicar datos en BD
+router.post('/post', (req, res) => {
+    const data = new Model({
+        name: req.body.name,
+        age: req.body.age
+    })
+})
+
+//Post Method
+router.post('/post', (req, res) => {
+    const data = new Model({
+        name: req.body.name,
+        age: req.body.age
+    })
+
+    try{
+
+    }
+    catch(error){
+
+    }
+})
+
+//Post Method
+router.post('/post', (req, res) => {
+    const data = new Model({
+        name: req.body.name,
+        age: req.body.age
+    })
+
+    try {
+        const dataToSave = data.save();
+        res.status(200).json(dataToSave)
+    }
+    catch (error) {
+        res.status(400).json({message: error.message})
+    }
+})
+
+router.post('/post', async (req, res) => {
+    const data = new Model({
+        name: req.body.name,
+        age: req.body.age
+    })
+
+    try {
+        const dataToSave = await data.save();
+        res.status(200).json(dataToSave)
+    }
+    catch (error) {
+        res.status(400).json({message: error.message})
+    }
+})
+
+router.get('/getAll', async (req, res) => {
+    try{
+        const data = await Model.find();
+        res.json(data)
+    }
+    catch(error){
+        res.status(500).json({message: error.message})
+    }
+})
+
+//Get by ID Method
+router.get('/getOne/:id', async (req, res) => {
+    try{
+        const data = await Model.findById(req.params.id);
+        res.json(data)
+    }
+    catch(error){
+        res.status(500).json({message: error.message})
+    }
+})
+
+//Update by ID Method
+router.patch('/update/:id', async (req, res) => {
+    try {
+        const id = req.params.id;
+        const updatedData = req.body;
+        const options = { new: true };
+
+        const result = await Model.findByIdAndUpdate(
+            id, updatedData, options
+        )
+
+        res.send(result)
+    }
+    catch (error) {
+        res.status(400).json({ message: error.message })
+    }
+})
+
+//Delete by ID Method
+router.delete('/delete/:id', async (req, res) => {
+    try {
+        const id = req.params.id;
+        const data = await Model.findByIdAndDelete(id)
+        res.send(`Document with ${data.name} has been deleted..`)
+    }
+    catch (error) {
+        res.status(400).json({ message: error.message })
+    }
 })
